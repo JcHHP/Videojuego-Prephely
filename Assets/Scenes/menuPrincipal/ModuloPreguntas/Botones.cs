ec13579004a41ecde286a11bb8542c02d727c205
Rodas <ruthds0975@gmail.com>
2023-02-15 11:34:12 -0500

    terrenos

---
+++ Assets/Scenes/menuPrincipal/ModuloPreguntas/Botones.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Botones: MonoBehaviour
+{
+    public List<Button> buttonsList;
+    private Button[] buttons;
+    public int num;
+    private void Start()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonsList.Add(buttons[i]);
+        }
+
+       // verifyIsPressed(false);
+
+    }
+    private void Update()
+    {
+
+    }
+
+    public void verifyIsPressed(bool verify)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonsList[i].interactable = verify;
+        }
+    }
+}
